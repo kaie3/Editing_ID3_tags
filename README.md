c0e00764a015f4f6ce63431735e625cabe7345a4
kaie3 <aaaaabbbbbcccccddddd@gmail.com>
2021-07-22 18:16:05 +0900

    方法変更

--- README.md
+++ README.md
@@ -5,14 +5,15 @@
 家族のiTunesなし古いiPod nanoから楽曲データを取り出してみたらID3タグ情報がなかったため追加する
 要望により出力ディレクトリ構造を[CD名/曲名.拡張子]にする
 
-## 楽曲データの取り出し方
+## 方法
 
 古のソフト,pod野郎をつかい以下の構成となるよう取り出した
 おそらくこうなっているはず
-歌手名/CD名/歌手名-曲名.拡張子
-CD名がなしときは[Music]となる
+歌手名/CD名/曲名.拡張子
+CD名がなしときは[不明なアルバム]とする
 !["directory structure"](./img/directory_structure.PNG)
-
-## 参考元
-
-["Pythonでmp3などのID3タグを編集するmutagenの使い方"](https://note.nkmk.me/python-mutagen-mp3-id3/)
+~~mutagenを使いタグ情報を付与する(["Pythonでmp3などのID3タグを編集するmutagenの使い方"](https://note.nkmk.me/python-mutagen-mp3-id3/))~~
+AIFFをうまく読み込んでくれないので方法を変える
+ディレクトリ構造を変更しBOM付きののテキストファイルを出力する
+["mp3tags"](https://www.mp3tag.de/en/)を使う
+["mp3tagsのMenu Convert > Text file - Tag機能"](https://help.mp3tag.de/main_converter.html#tff)を使用する
--- main.py
+++ main.py
@@ -1,61 +1,7 @@
 import os
 import pathlib
-import re
-import shutil
 import pprint
-
-from mutagen.aiff import AIFF
-
-
-def set_id3_tag(
-    file_path: str,
-    title=None,
-    artist=None,
-    albumartist=None,
-    album=None,
-    genre=None,
-    track_num=None,
-    total_track_num=None,
-    disc_num=None,
-    total_disc_num=None,
-):
-    print(file_path)
-    tags = AIFF(file_path)
-    pprint.pprint(tags)
-    if title:
-        tags["title"] = title
-    if artist:
-        tags["artist"] = artist
-    if albumartist:
-        tags["albumartist"] = albumartist
-    if album:
-        tags["album"] = album
-    if genre:
-        tags["genre"] = genre
-    if total_track_num:
-        if track_num:
-            tags["tracknumber"] = "{}/{}".format(track_num, total_track_num)
-        else:
-            tags["tracknumber"] = "/{}".format(total_track_num)
-    else:
-        if track_num:
-            tags["tracknumber"] = "{}".format(track_num)
-    if total_disc_num:
-        if disc_num:
-            tags["discnumber"] = "{}/{}".format(disc_num, total_disc_num)
-        else:
-            tags["discnumber"] = "/{}".format(total_disc_num)
-    else:
-        if track_num:
-            tags["discnumber"] = "{}".format(disc_num)
-
-    # tags.save()
-
-
-def show_id3_tags(file_path):
-    tags = AIFF(file_path)
-    print(tags.pprint())
-
+import shutil
 
 # pathから各種データ取得
 def get_data_from_paths(paths):
@@ -67,16 +13,21 @@ def get_data_from_paths(paths):
                 .replace(str(path.parents[1]) + "/", "")
                 .replace("　", " ")  # 全角スペース
                 .replace("  ", " ")  # 半角スペース2個
+                .replace("～", "~")
             )
             artist = (
                 str(path.parents[1])
                 .replace(str(path.parents[2]) + "/", "")
                 .replace("　", " ")  # 全角スペース
                 .replace("  ", " ")  # 半角スペース2個
+                .replace("～", "~")
             )
             title = (
-                re.sub(r".+-", "", path.stem).replace("　", " ").replace("  ", " ")
-            )  # 全角スペース,半角スペース2個
+                str(path.stem)
+                .replace("　", " ")  # 全角スペース
+                .replace("  ", " ")  # 半角スペース2個
+                .replace("～", "~")
+            )
             extension = path.suffix
             paths_data.append((str(path), album, artist, title, extension))
     return paths_data
@@ -92,18 +43,26 @@ def make_out_file(paths_data):
         if not os.path.isfile(new_dir_path + title):  # ファイルの存在確認
             # shutil.copy2(path, new_dir_path + title + extension)
             print("fileコピー中:" + new_dir_path + title + extension)
-            id3_tags.append((new_dir_path + title + extension, album, artist, title))
+            new_file_path = new_dir_path + title + extension
+            new_file_path = new_file_path.replace("/", "\\")
+            id3_tags.append((new_file_path, album, artist, title))
 
     print("コピー完了!")
     return id3_tags
+
+
+def writing_to_txt(id3_tags):
+    tags = []
+    # %_filename_ext% / %album% / %artist% / %title%
+    for new_file_path, album, artist, title in id3_tags:
+        tags.append(f"{new_file_path}/{album}/{artist}/{title}")
+
+    with open("./tags.txt", mode="w", encoding="utf_8_sig") as f:
+        f.writelines("\n".join(tags))
 
 
 # サブディレクトリも含めてすべてのファイルパスを取得
 paths = list(pathlib.Path("./scr").glob("**/*"))
 paths_data = get_data_from_paths((paths))
 id3_tags = make_out_file(paths_data)
-for path, album, artist, title in id3_tags:
-    set_id3_tag(file_path=path, title=title, artist=artist, album=album)
-# 結果表示
-for path, _album, _artist, _title in id3_tags:
-    show_id3_tags(path)
+writing_to_txt(id3_tags)
